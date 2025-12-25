3f19564c082d2869b9769dfb8ad055f77cbe4295
AVSK-G <avishekpaal@gmail.com>
2025-12-25 15:27:23 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = "pwa-game-v5";
+const CACHE_NAME = "pwa-game-v1";
 
-const urlsToCache = [
-  "/pwa-game/",
-  "/pwa-game/index.html",
-  "/pwa-game/manifest.json",
-  "/pwa-game/icon-192.png",
-  "/pwa-game/icon-512.png"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
